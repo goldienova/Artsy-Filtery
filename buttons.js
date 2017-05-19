309cf49826d2ed323443dda4db1fe1cae5e026d0
Mariana Templin <mariana.acuna.templin@gmail.com>
2017-05-19 11:52:34 -0400

    old website with artsy filtery added to site

--- buttons.js
+++ buttons.js
@@ -17,6 +17,7 @@ function drawButtons(){
 
     var imgToConvert = ctx.getImageData(0, 0, 1059, 800);
     var resetImg = ctx.getImageData(0, 0, 1059, 800);
+    console.log(imgToConvert);
 
     var imgData = imgToConvert.data;
     var resetData = resetImg.data;
@@ -35,7 +36,7 @@ function drawButtons(){
       console.log("invert")
     };
 
-  var a = function(){
+  var pinkify = function(){
     for (var i = 0; i < imgData.length; i += 4) {
         imgData[i]     = 255 - imgData[i];     // red
         imgData[i + 1] = 255 - imgData[i + 1]; // green
@@ -46,7 +47,7 @@ function drawButtons(){
 
     }
 
-    var b = function(){
+    var highlighter = function(){
       for (var i = 0; i < imgData.length; i += 4) {
        imgData[i]     = 225 - imgData[i];     // red
        imgData[i + 1] = 225 - imgData[i + 1]; // green
@@ -56,7 +57,7 @@ function drawButtons(){
      ctx.putImageData(imgToConvert, 0, 0);
    }
 
-   var c = function(){
+   var snow = function(){
     var backwards = 0;
     var forwards = 0;
     for(var i = 0; i < imgData.length; i += 1059){
@@ -70,15 +71,15 @@ function drawButtons(){
 
   }
 
-  var d = function(){
+  var sand = function(){
     for (var i = 0; i < imgData.length; i += 4) {
       //console.log("BEFORE", imgData[i])
-      imgData[i] = 255 - Math.random() * resetData[i];
+      imgData[i] = 255 - Math.random() * imgData[i];
       //console.log("AFTER", imgData[i])
       // imgData[i + 1] = resetData[i +1] - (Math.random() * (255 - 153) + 100);
       // imgData[i + 2] = resetData[i + 2] - Math.random() * 204;
-      imgData[i+1] = 255 - Math.random() * resetData[i+1];
-      imgData[i+2] = 255 - Math.random() * resetData[i+2];
+      imgData[i+1] = 255 - Math.random() * imgData[i+1];
+      imgData[i+2] = 255 - Math.random() * imgData[i+2];
     }
     ctx.putImageData(imgToConvert, 0, 0);
 
@@ -88,7 +89,7 @@ function drawButtons(){
   //Math.random() * (Max - Min) + Min
   //resetData[i]  === num btwn 0-255
 
-  var e = function(){
+  var coogi = function(){
     var backwards = 0;
     var forwards = 0;
     for(var i = 0; i < imgData.length; i += width){
@@ -124,7 +125,7 @@ function drawButtons(){
 
   }
 
-  var f = function(){
+  var stripes = function(){
 
     for(var i = imgData.length; i > 0; i -= (width/3 -1)){
       for(var x=0; x<width; x+=32){
@@ -142,42 +143,59 @@ function drawButtons(){
 
   var g = function(){
     for (var i = 0; i < imgData.length; i += 4) {
-      //console.log("BEFORE", imgData[i])
       imgData[i] = Math.random() * 255 + Math.random() * resetData[i];
-      //console.log("AFTER", imgData[i])
-      // imgData[i + 1] = resetData[i +1] - (Math.random() * (255 - 153) + 100);
-      // imgData[i + 2] = resetData[i + 2] - Math.random() * 204;
       imgData[i+1] = Math.random() * 255 + Math.random() * resetData[i+1];
       imgData[i+2] = Math.random() * 255 - Math.random() * resetData[i+2];
     }
     ctx.putImageData(imgToConvert, 0, 0);
 
-    console.log("experiment")
   }
 
 
-  var h = function(){
+  var lisaFrank = function(){
 
     for(var i = 0; i < imgData.length; i += width ){
       for(var x=0; x<width; x+=4){
-        //console.log('hi')
         imgData[i + x] = 204 - imgData[i + x];
         imgData[i + x + 1] = imgData[i + x + 1];
         imgData[i + x + 2] = 204 - imgData[i + x + 2];
       }
     }
-    console.log("in h")
 
     ctx.putImageData(imgToConvert, 0, 0)  
 
   }
 
-  // var g = setInterval(f, 2000)
+
+var autoplay;
+var on = true;
+
+  var static = function(){
+  
+  if(on){
+    autoplay = setInterval(g, 200)
+    on = false;
+    } else{
+      clearInterval(autoplay)
+      reset();
+      on = true;
+    }
+   
+  }
+
+
+  var invert = function() {
+      for (var i = 0; i < imgData.length; i += 4) {
+      imgData[i]     = 255 - imgData[i];     // red
+      imgData[i + 1] = 255 - imgData[i + 1]; // green
+      imgData[i + 2] = 255 - imgData[i + 2]; // blue
+    }
+    ctx.putImageData(imgToConvert, 0, 0);
+  };
+
 
 
   var reset = function() {
-
-    console.log("BEFORE: imgData", imgData, "resetData", resetData)
 
     for (var i = 0; i < imgData.length; i += 4) {
       imgData[i] = resetData[i];
@@ -185,42 +203,40 @@ function drawButtons(){
       imgData[i + 2] = resetData[i + 2];
       imgData[i + 4] = resetData[i + 4];
     }
-    console.log("AFTER: imgData", imgData, "resetData", resetData)
     
     ctx.putImageData(resetImg, 0, 0);
+    clearInterval(autoplay);
 
   };
 
-  var invertbtn = document.getElementById('a');
+  var invertbtn = document.getElementById('pinkify');
   console.log(invertbtn)
-  invertbtn.addEventListener('click', a);
+  invertbtn.addEventListener('click', pinkify);
 
-  var btn = document.getElementById('b');
-  btn.addEventListener('click', b);
+  var btn = document.getElementById('highlighter');
+  btn.addEventListener('click', highlighter);
 
-  var btn = document.getElementById('c');
-  btn.addEventListener('click', c) 
+  var btn = document.getElementById('snow');
+  btn.addEventListener('click', snow) 
 
-  var btn = document.getElementById('d');
-  btn.addEventListener('click', d) 
+  var btn = document.getElementById('sand');
+  btn.addEventListener('click', sand) 
 
-  var btn = document.getElementById('e');
-  btn.addEventListener('click', e)
+  var btn = document.getElementById('coogi');
+  btn.addEventListener('click', coogi)
 
-  var btn = document.getElementById('f');
-  btn.addEventListener('click', f)
+  var btn = document.getElementById('stripes');
+  btn.addEventListener('click', stripes)
 
-  var btn = document.getElementById('g');
-  btn.addEventListener('click', g)
 
-  var btn = document.getElementById('h');
-  btn.addEventListener('click', h)
+  var btn = document.getElementById('Lisa Frank');
+  btn.addEventListener('click', lisaFrank)
 
-  var btn = document.getElementById('i');
-  btn.addEventListener('click', i)
+  var btn = document.getElementById('static');
+  btn.addEventListener('click', static)
 
-  var btn = document.getElementById('j');
-  btn.addEventListener('click', j)
+  var invertbtn = document.getElementById('invert');
+  invertbtn.addEventListener('click', invert);
 
   var btn = document.getElementById('reset');
   btn.addEventListener('click', reset)  
@@ -230,12 +246,5 @@ function drawButtons(){
 
 }
 
-
-
-
-
-
-  // var grayscalebtn = document.getElementById('grayscalebtn');
-  // grayscalebtn.addEventListener('click', grayscale);
 
   window.onload = drawButtons;
